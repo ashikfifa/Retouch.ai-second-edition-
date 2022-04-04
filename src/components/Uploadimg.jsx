635bb37c2c673f8a74064ef3cd824facf4802ae1
Md. Asiquzzaman <asiquzzaman@thekowcompany.com>
2022-04-04 13:06:13 +0600

    changes upload image

--- src/components/Uploadimg.jsx
+++ src/components/Uploadimg.jsx
@@ -1,70 +1,110 @@
-import React from "react";
-import { Card, CardHeader, CardBody, CardFooter } from 'react-simple-card';
+import React from "react"
 
 function Uploadimg() {
   return (
  <div className="uploadimg">
 <div class="container">
-    <div class="row">
+    <div class="row" style={{marginTop:"3%",marginBottom:"3%"}}>
 
         <div class="col-md-3">
- <div class="card" style={{width: "100%",borderRadius:"10px"}}>
+ <div class="card">
     <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+     <div class="row">
+         <div class="col-md-4">
+        <img src="assets/images/ba-08.png"  height= "68px" alt=""/>
+        </div>
+        <div class="col-md-8">
+        <p class="card-text">Background Remove</p>
+         </div>
+     </div>
     </div>
  </div>
         </div>
 
         <div class="col-md-3">
- <div class="card" style={{width: "100%",borderRadius:"10px"}}>
+        <div class="card">
     <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+     <div class="row">
+         <div class="col-md-4">
+        <img src="assets/images/ba-08.png"  height= "68px" alt=""/>
+        </div>
+        <div class="col-md-8">
+        <p class="card-text">Background Remove</p>
+         </div>
+     </div>
     </div>
  </div>
         </div>
 
 
         <div class="col-md-3">
- <div class="card" style={{width: "100%",borderRadius:"10px"}}>
+        <div class="card">
     <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+     <div class="row">
+         <div class="col-md-4">
+        <img src="assets/images/ba-08.png"  height= "68px" alt=""/>
+        </div>
+        <div class="col-md-8">
+        <p class="card-text">Background Remove</p>
+         </div>
+     </div>
     </div>
  </div>
         </div>
 
 
         <div class="col-md-3">
- <div class="card" style={{width: "100%",borderRadius:"10px"}}>
+        <div class="card">
     <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+     <div class="row">
+         <div class="col-md-4">
+        <img src="assets/images/ba-08.png"  height= "68px" alt=""/>
+        </div>
+        <div class="col-md-8">
+        <p class="card-text">Background Remove</p>
+         </div>
+     </div>
     </div>
  </div>
         </div>
 </div>
 
 
+
+
+
+
+
+
 <div class="card" style={{width: "100%",borderRadius:"10px",backgroundColor:"#f3f5f7"}}>
     <div class="card-body">
     <div class="row">
+
+
 <div class="col-md-8">
 <div class="card">
-<div style={{padding:"23% 23%",backgroundColor:"white"}}>
+<div style={{padding:"13% 13%",backgroundColor:"white"}}>
         <p style={{textAlign:"center",fontSize:"25px"}} class="card-text">Select a service and <br/>upload your images</p>
-        <i class="fa fa-cloud-upload" style={{fontSize:"80px",marginLeft:"40%"}}></i> <br/><br/>
-        <div class="text-center">
-        <button id="uploadbtn">Upload</button>
+       
+       <div class="text-center">
+        <div class="upload-btn-wrapper">
+                 <i class="fa fa-cloud-upload" style={{fontSize:"80px"}}></i> <br/>
+                
+            <button class="uploadbtn">Upload a file</button>
+            <input type="file" name="myfile" />
         </div>
- </div>      
         </div>
+
+   </div>      
+</div>
+<br/>
+    <div class="text-center">
+        <button style={{border:"none",padding:"5px 38px",backgroundColor:"red",color:"white",borderRadius:"15px"}}>START</button>
     </div>
+
+</div>
+
+    
 
  <div class="col-md-4">
  <div style={{padding:"50% 25%"}}>
@@ -76,6 +116,9 @@ function Uploadimg() {
 </div>
 </div>
  </div>
+
+
+
 
 
 
